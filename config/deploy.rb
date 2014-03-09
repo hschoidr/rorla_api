66a165ae43667beb740918b614b5dbacdde7d1d2
Hyo Seong Choi <rorlab@gmail.com>
2014-03-09 19:51:51 +0900

    untraced database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,10 @@ set :branch, :cap3
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/home/deployer/apps/rorla_api'
 
+set :default_env, {
+  'PATH' => "~/.rbenv/shims:~/.rbenv/bin:$PATH"
+}
+ 
 # Default value for :scm is :git
 # set :scm, :git
 
@@ -38,13 +42,10 @@ set :deploy_to, '/home/deployer/apps/rorla_api'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-set :rbenv_type, :user # or :system, depends on your rbenv setup
+# set :rbenv_type, :user # or :system, depends on your rbenv setup
 set :rbenv_ruby, '2.1.1'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
-set :rbenv_roles, :all # default value
 
-Dir.glob('config/deploy/recipes/*.rb').each { |recipe| load recipe }
+# Dir.glob('config/deploy/recipes/*.rb').each { |recipe| load recipe }
 
 namespace :deploy do
 
